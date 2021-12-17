30db7a21f5531b2a4bea1801d77879285456d27d
Abdullah Adeel <easyautosol@gmail.com>
2021-12-17 22:42:55 +0500

    added websocket manager

--- backend/app/websocket/handlers/docs.py
+++ backend/app/websocket/handlers/docs.py
@@ -1,12 +1,18 @@
 from fastapi import APIRouter, WebSocket
-
+from starlette.websockets import WebSocketDisconnect
+from app.websocket.utils import manager as ws_manager
 
 docs_router = APIRouter()
 
 
 @docs_router.websocket("/", "websocket.docs")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await ws_manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await ws_manager.send_personal_message(f"You wrote: {data}", websocket)
+            await ws_manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        ws_manager.disconnect(websocket)
+        await ws_manager.broadcast(f"Client #{client_id} left the chat")
